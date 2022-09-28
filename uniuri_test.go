9b1c754cea3676b8413e9602b23e6105c0496b49
Dmitry Chestnykh <dmitry@codingrobots.com>
2022-09-28 22:35:59 +0200

    Add benchmarks

--- uniuri_test.go
+++ uniuri_test.go
@@ -100,3 +100,57 @@ func TestBias(t *testing.T) {
 		}
 	}
 }
+
+var (
+	sixtyFourChars = append(StdChars, []byte{'+', '/'}...)
+	sixtyFiveChars = append(sixtyFourChars, []byte{'.'}...)
+	threeChars     = []byte{'a', 'b', 'c'}
+)
+
+func BenchmarkLen16Chars65(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(StdLen, sixtyFiveChars)
+	}
+}
+
+func BenchmarkLen16Chars64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(StdLen, sixtyFourChars)
+	}
+}
+
+func BenchmarkLen16Chars62(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(StdLen, StdChars)
+	}
+}
+
+func BenchmarkLen16Chars3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(StdLen, threeChars)
+	}
+}
+
+func BenchmarkLen1024Chars65(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(1024, sixtyFiveChars)
+	}
+}
+
+func BenchmarkLen1024Chars64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(1024, sixtyFourChars)
+	}
+}
+
+func BenchmarkLen1024Chars62(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(1024, StdChars)
+	}
+}
+
+func BenchmarkLen1024Chars3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = NewLenChars(1024, threeChars)
+	}
+}
